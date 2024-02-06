9eebe2949078acc291ed426c9ec7c8ddb20879b6
WendellAndrade <wendell101ferreira@icloud.com>
2024-02-05 23:36:28 -0300

    Frist  Steps in java with Spring Boot 3

---
+++ 05_FristStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/GreetingController.java
@@ -0,0 +1,25 @@
+package br.com.erudio;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController	
+public class GreetingController {
+
+	private static final String tamplate = "Hello, %s!";
+	private AtomicLong counter  = new AtomicLong();
+	
+	@RequestMapping("/greeting")
+	public Greeting greeting(
+			@RequestParam(value = "name", defaultValue = "Word") 
+			String name) {
+		
+		return new Greeting(counter.incrementAndGet(), String.format(tamplate, name));
+		
+	}
+	
+
+}
