63bb8bdfedb4ea441aef0e224623b5d1db4918d8
WendellAndrade <wendell101ferreira@icloud.com>
2025-08-28 23:40:33 -0300

    Atualizando projeto

---
+++ 11_MigrationsWithFlyWay/src/main/resources/db/migration/V2__Create_Person.sql
@@ -0,0 +1,7 @@
+-- Copiando dados para a tabela rest_with_spring_boot_erudio.person: ~5 rows (aproximadamente)
+INSERT INTO `person` (`id`, `andress`, `frist_name`, `gender`, `last_name`) VALUES
+	(4, 'João Pessoa - PB', 'Wendell', 'Male', 'Andrade'),
+	(5, 'João Pessoa - PB - Brasil', 'Eduarda', 'Female', 'Corredora 01'),
+	(6, 'Patos - PB', 'José Winicius', 'Male', 'Andrade'),
+	(7, 'João Pessoa - PB - Brasil', 'Sarah', 'Female', 'Corredora 02'),
+	(8, 'Todo Canto', 'Jesus', 'Male', 'Cristo');
